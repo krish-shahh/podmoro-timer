4ccaf3b53f8b7a1b051218fd7775b1cbddd4d6a2
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2022-01-17 18:47:07 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Pomodoro Timer
+A Simple timer to boost your productivity
+
+> The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks. These intervals are named pomodoros, the plural in English of the Italian word pomodoro (tomato), after the tomato-shaped kitchen timer that Cirillo used as a university student.  
+https://en.wikipedia.org/wiki/Pomodoro_Technique
+
+How does it work ?  
+
+1. Start a timer
+2. Work until the timer rings
+3. Take a short break
+4. Every 4 pomodoros (work periods), take a longer break
+
+## Demo
+https://romaindso.github.io/pomodoro-timer/
+
+## Features
+
+* PWA capacities, works offline 
+* Material design with material-ui
+
+
+## Installation
+Clone this project, then run in your console :
+```bash
+$ npm install
+```
+
+## Launch
+Dev mode
+```bash
+$ npm start
+```
+
+Prod mode
+```bash
+$ npm run build
+```
---
+++ src/Timer.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ActionPlay from 'material-ui/svg-icons/av/play-arrow';
+import ActionPause from 'material-ui/svg-icons/av/pause';
+import ActionReset from 'material-ui/svg-icons/av/replay';
+import './Timer.css';
+
+const TIMER_25 = 25 * 60 * 1000;
+
+export default class Timer extends Component {
+
+  constructor(props){
+    super(props);
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.reset = this.reset.bind(this);
+    this.state = {
+      time: TIMER_25
+    }
+  }
+
+  start(){
+    if(!this._interval){
+      this._interval = setInterval(() => {
+        if(this.state.time !== 0){
+          this.setState({
+            time: this.state.time - 1000,
+          });
+        } else {
+          let audio = new Audio('BikeHorn.mp3');
+          audio.play();
+          this.clear();
+        }
+      }, 1000);
+    }
+  }
+
+  stop(){
+    this.clear();
+  }
+
+  clear(){
+    if(this._interval){
+      clearInterval(this._interval);
+      this._interval = null;
+    }
+  }
+
+  reset(){
+    this.clear();
+    this.setState({
+      time: TIMER_25
+    })
+  }
+
+  addZero(number){
+    return number < 10 ? '0' + number : number;
+  }
+
+  render (){
+    let time = this.state.time;
+    let seconds = Math.floor( (time/1000) % 60 );
+    let minutes = Math.floor( (time/1000/60) % 60 );
+    let completed = 100 * (time / TIMER_25);
+
+    return (
+      <div className="Timer">
+        <div className="clock-wrapper">
+          <CircularProgress
+            mode="determinate"
+            value={completed}
+            size={250}
+            thickness={15}
+            color="#A12B12"
+          />
+          <div className="time-wrapper">
+            <p>{this.addZero(minutes)}:{this.addZero(seconds)}</p>
+          </div>
+        </div>
+        <footer>
+        <FloatingActionButton onClick={this.start} className="action-button" backgroundColor="#A12B12">
+          <ActionPlay />
+        </FloatingActionButton>
+        <FloatingActionButton onClick={this.stop} className="action-button" backgroundColor="#A12B12">
+          <ActionPause />
+        </FloatingActionButton>
+        <FloatingActionButton onClick={this.reset} className="action-button" backgroundColor="#A12B12">
+          <ActionReset />
+        </FloatingActionButton>
+        </footer>
+      </div>
+    )
+  }
+}
